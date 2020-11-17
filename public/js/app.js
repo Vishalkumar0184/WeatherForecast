d7142c5082fd2b0e965d80f4558c4c2b072052eb
vishalkumar0184 <kumarvishal.16997@gmail.com>
2020-11-18 05:16:07 +0530

    app.js and index.hbs update

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
-const weatherForm = document.querySelector("form");
-const search = document.querySelector("input");
+const cityNameHandler = document.querySelector("#cityName");
+const cityIdHandler = document.querySelector("#cityId");
+const cityLatHandler = document.querySelector("#cityLat");
+const cityZipHandler = document.querySelector("#cityZip");
+
 const messageOne = document.querySelector("#message-1")
 const messageTwo = document.querySelector("#message-2")
 
@@ -14,6 +17,16 @@ const cityLatLabel = document.querySelector('#cityLat')
 const cityLongLabel = document.querySelector('#cityLong')
 const cityZipLabel = document.querySelector('#cityZip')
 
+const cityidDatalabel = document.querySelector('#cityidData')
+const cityNameDatalabel = document.querySelector('#cityNameData')
+const latitudeDatalabel = document.querySelector('#latitudeData')
+const longitudeDatalabel = document.querySelector('#longitudeData')
+const currentDatalabel = document.querySelector('#currentData')
+const minDatalabel = document.querySelector('#minData')
+const maxDatalabel = document.querySelector('#maxData')
+const pressureDatalabel = document.querySelector('#pressureData')
+const humidityDatalabel = document.querySelector('#humidityData')
+
 console.log("Button", cityNameButton, cityNameLabel)
 
 cityNameButton.addEventListener("click", () => {
@@ -22,6 +35,7 @@ cityNameButton.addEventListener("click", () => {
     cityIdLabel.className = "displayNone";
     cityLatLabel.className = "displayNone";
     cityZipLabel.className = "displayNone";
+    messageTwo.className = "displayNone"
 })
 cityIdButton.addEventListener("click", () => {
     console.log("cityIdButton clicked")
@@ -29,6 +43,7 @@ cityIdButton.addEventListener("click", () => {
     cityIdLabel.className = "displayInline";
     cityLatLabel.className = "displayNone";
     cityZipLabel.className = "displayNone";
+    messageTwo.className = "displayNone"
 })
 cityCoordinateButton.addEventListener("click", () => {
     console.log("cityCoordinateButton clicked")
@@ -36,6 +51,7 @@ cityCoordinateButton.addEventListener("click", () => {
     cityIdLabel.className = "displayNone";
     cityLatLabel.className = "displayInline";
     cityZipLabel.className = "displayNone";
+    messageTwo.className = "displayNone"
 })
 cityZipButton.addEventListener("click", () => {
     console.log("cityNameButton clicked")
@@ -43,31 +59,68 @@ cityZipButton.addEventListener("click", () => {
     cityIdLabel.className = "displayNone";
     cityLatLabel.className = "displayNone";
     cityZipLabel.className = "displayInline";
+    messageTwo.className = "displayNone"
 })
 
+console.log("cityNameHandler", cityNameHandler)
 
-
-weatherForm.addEventListener("submit", (e) => {
-
+cityNameHandler.addEventListener("submit", (e) => {
     e.preventDefault();
+    submitHandler(e);
+})
 
-    const location = search.value
+cityIdHandler.addEventListener("submit", (e) => {
+    e.preventDefault();
+    submitHandler(e);
+})
+
+cityLatHandler.addEventListener("submit", (e) => {
+    e.preventDefault();
+    submitHandler(e);
+})
+
+cityZipHandler.addEventListener("submit", (e) => {
+    e.preventDefault();
+    submitHandler(e);
+})
+
+function submitHandler(event) {
+    console.log("submitHandler called", event.target)
+    console.log('target ', event.target)
+    let url = '';
     messageOne.textContent = "Loading..."
-    messageTwo.textContent = ""
+    if (event.target.q) {
+        url = "http://localhost:3000/v1/user/getWeather?q=" + event.target.q.value;
+    } else if (event.target.userId) {
+        url = "http://localhost:3000/v1/user/getWeather?id=" + event.target.userId.value;
+    } else if (event.target.zip) {
+        url = "http://localhost:3000/v1/user/getWeather?zip=" + event.target.zip.value;
+    } else if (event.target.lat && event.target.lon) {
+        url = "http://localhost:3000/v1/user/getWeather?lat=" + event.target.lat.value + "&lon=" + event.target.lon.value;
+    }
 
-
-    fetch("https://weatherapiforecast.herokuapp.com?q=" + location).then((response) => {
+    fetch(url).then((response) => {
 
         response.json().then((data) => {
-
+            console.log('data', data)
             if (data.error) {
                 messageOne.textContent = (data.error)
-            }
-            else {
-                console.log("Data sent Sucessfully")
+            } else {
+                console.log("messageTwo")
+                messageTwo.className = "displayInline";
+                messageOne.textContent = "";
+                cityidDatalabel.innerHTML = data.result.cityId;
+                cityNameDatalabel.innerHTML = data.result.cityName
+                latitudeDatalabel.innerHTML = data.result.cordinate.latitude;
+                longitudeDatalabel.innerHTML = data.result.cordinate.longitude;
+                currentDatalabel.innerHTML = data.result.temprature.current;
+                minDatalabel.innerHTML = data.result.temprature.minimum;
+                maxDatalabel.innerHTML = data.result.temprature.maximum;
+                pressureDatalabel.innerHTML = data.result.temprature.pressure;
+                humidityDatalabel.innerHTML = data.result.temprature.humidity;
+                console.log("Data sent Sucessfully", data.result)
             }
         })
 
     })
-
-})
+}
--- router/router.js
+++ router/router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const webService = require('../webService/webService');
 
-router.get('https://weatherapiforecast.herokuapp.com', webService.getWeather)
+router.get('/getWeather', webService.getWeather)
 
 module.exports = router;
